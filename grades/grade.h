29d99077898c8467946d6079decec7183c753fa5
wooken <kenny@kenwoo.io>
2018-01-02 23:43:21 -0800

    Add: Grades program from chapter 4

---
+++ grades/grade.h
@@ -0,0 +1,12 @@
+#ifndef GRADE_H
+#define GRADE_H
+
+#include <vector>
+
+#include "Student_info.h"
+
+double grade(double, double, double);
+double grade(double, double, const std::vector<double> &);
+double grade(const Student_info &);
+
+#endif
---
+++ grades/median.cpp
@@ -0,0 +1,22 @@
+#include <algorithm> // sort
+#include <stdexcept> // domain_error
+#include <vector> // vector
+
+using std::domain_error;
+using std::sort;
+using std::vector;
+
+// compute the median of a vector<double>
+// note that calling this function copies the entire argument vector
+double median(vector<double> vec) {
+    typedef vector<double>::size_type vec_sz;
+
+    vec_sz size = vec.size();
+    if (size == 0)
+        throw domain_error("median of an empty vector");
+
+    sort(vec.begin(), vec.end());
+
+    vec_sz mid = size / 2;
+    return size % 2 == 0 ? (vec[mid] + vec[mid-1]) / 2 : vec[mid];
+}
