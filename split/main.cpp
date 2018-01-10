9f2b8c5d132c69ed7690857f230badf7a3942d0d
wooken <kenny@kenwoo.io>
2018-01-09 17:20:57 -0800

    Add third version of split function
    The third version uses:
    - iterators
    - find_if() from <algorithm>, which iterates through a string until the
    predicate is true and returns an iterator where it stopped

--- split/main.cpp
+++ split/main.cpp
@@ -1,14 +1,17 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <algorithm> // find_if
 
 using std::cin;
 using std::cout;
 using std::endl;
+using std::find_if;
 using std::string;
 using std::vector;
 
-vector<string> split(const string &s) {
+// split_func1() code ---------------------------------------------------------
+vector<string> split_func1_helper(const string &s) {
     vector<string> ret;
     typedef string::size_type string_size;
     string_size i = 0;
@@ -35,11 +38,11 @@ vector<string> split(const string &s) {
     return ret;
 }
 
-void main1() {
+void split_func1() {
     string s;
     // read and split each line of input
     while (getline(cin, s)) {
-        vector<string> v = split(s);
+        vector<string> v = split_func1_helper(s);
         // write each word in v
         for (vector<string>::size_type i = 0; i != v.size(); i++) {
             cout << v[i] << endl;
@@ -47,7 +50,52 @@ void main1() {
     }
 }
 
-void main2() {
+// end split_func1() code -----------------------------------------------------
+
+// split_func3() code ---------------------------------------------------------
+bool space(char c) {
+    return isspace(c);
+}
+
+bool not_space(char c) {
+    return !isspace(c);
+}
+
+// uses iterators and find_if() from <algorithm>
+vector<string> split_func3_helper(const string &str) {
+    typedef string::const_iterator iter;
+    vector<string> ret;
+
+    iter i = str.begin();
+    while (i != str.end()) {
+        // ignore leading blanks
+        i = find_if(i, str.end(), not_space);
+
+        // find end of next word
+        iter j = find_if(i, str.end(), space);
+
+        // copy the characters in [i, j)
+        if (i != str.end())
+            ret.push_back(string(i, j));
+        i = j;
+    }
+    return ret;
+}
+
+void split_func3() {
+    string s;
+    // read and split each line of input
+    while (getline(cin, s)) {
+        vector<string> v = split_func3_helper(s);
+        // write each word in v
+        for (vector<string>::size_type i = 0; i != v.size(); i++) {
+            cout << v[i] << endl;
+        }
+    }
+}
+// end split_func3() code -----------------------------------------------------
+
+void split_func2() {
     string s;
     while (cin >> s) {
         cout << s << endl;
@@ -55,7 +103,8 @@ void main2() {
 }
 
 int main() {
-    main1();
-    //main2();
+    //split_func1();
+    //split_func2();
+    split_func3();
     return 0;
 }
